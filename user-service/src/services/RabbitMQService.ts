0e8968e515f34608fff9e7f98a89154cabb90b9b
Ali Bugra <akdoganalibugra@gmail.com>
2025-05-14 02:06:39 +0300

    feat: initialize user service with authentication and RabbitMQ integration
    - Added package.json for user service with necessary dependencies.
    - Implemented configuration management using dotenv.
    - Created AuthController for user registration and login functionalities.
    - Established MongoDB connection and user model with validation.
    - Developed middleware for error handling.
    - Set up authentication routes for user registration and login.
    - Built the main server file to initialize the application and connect to RabbitMQ.
    - Implemented RabbitMQ service for handling user detail requests.
    - Added utility functions for password encryption and error handling.
    - Configured TypeScript settings for the project.

---
+++ user-service/src/services/RabbitMQService.ts
@@ -0,0 +1,55 @@
+import * as amqp from "amqplib";
+import { Connection, Channel, ConsumeMessage } from "amqplib";
+import config from "../config/config";
+import { User } from "../database";
+import { ApiError } from "../utils";
+
+class RabbitMQService {
+  private requestQueue = "USER_DETAILS_REQUEST";
+  private responseQueue = "USER_DETAILS_RESPONSE";
+  private connection: any;
+  private channel: any;
+
+  constructor() {
+    this.init();
+  }
+
+  async init() {
+    // @ts-ignore: using any to bypass type mismatch in amqplib
+    this.connection = await amqp.connect(config.msgBrokerURL!);
+    // @ts-ignore: using any to bypass type mismatch in amqplib
+    this.channel = await this.connection.createChannel();
+
+    // Asserting queues ensures they exist
+    await this.channel.assertQueue(this.requestQueue);
+    await this.channel.assertQueue(this.responseQueue);
+
+    // Start listening for messages on the request queue
+    this.channel.consume(this.requestQueue, async (msg: any) => {
+      if (msg && msg.content) {
+        const { userId } = JSON.parse(msg.content.toString());
+        const userDetails = await getUserDetails(userId);
+
+        // Send the user details response
+        this.channel.sendToQueue(
+          this.responseQueue,
+          Buffer.from(JSON.stringify(userDetails)),
+          { correlationId: msg.properties.correlationId }
+        );
+
+        // Acknowledge the processed message
+        this.channel.ack(msg);
+      }
+    });
+  }
+}
+
+const getUserDetails = async (userId: string) => {
+  const userDetails = await User.findById(userId).select("-password");
+  if (!userDetails) {
+    throw new ApiError(404, "User not found");
+  }
+
+  return userDetails;
+};
+export const rabbitMQService = new RabbitMQService();
